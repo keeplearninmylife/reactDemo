2fbb0e279c55f04e2b3578d86b03b460b5bf2ad1
keeplearninmylife <3347678815@qq.com>
2020-04-05 23:48:11 +0800

    commit message

---
+++ src/Xiaojiejie.js
@@ -0,0 +1,18 @@
+import React,{ Component, Fragment} from "react"
+
+class Xiaojiejie extends Component{
+    render(){
+        return (
+            <Fragment>
+                <div><input/><button>增加服务</button></div>
+                <ul>
+                    <li>脚底按摩</li>
+                    <li>脚底按摩</li>
+                    <li>脚底按摩</li>
+                </ul>
+            </Fragment>
+        )
+    }
+}
+
+export default Xiaojiejie
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from "react"
+import ReactDom from "react-dom"
+import Xiaojiejie from "./Xiaojiejie"
+
+ReactDom.render(<Xiaojiejie/>,document.getElementById("root"))
